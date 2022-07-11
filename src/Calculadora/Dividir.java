76000b9c7d67d87dabad493c1d7717642cb636fe
Renilde-Ferreira <renilde134@gmail.com>
2022-07-11 15:23:40 -0300

    inicializando

---
+++ src/Calculadora/Dividir.java
@@ -0,0 +1,11 @@
+package Calculadora;
+
+
+
+public class Dividir {
+    public static void main(String[] args) {
+        int n = 8, b = 2;
+        int divide = n / b;
+        System.out.println(divide);
+    }
+}
