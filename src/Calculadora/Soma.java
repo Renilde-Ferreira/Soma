57d3cfbe12fd6b8da7262d70a14220434521925c
Renilde-Ferreira <renilde134@gmail.com>
2022-07-11 15:15:38 -0300

    Programa inicial

---
+++ src/Calculadora/Soma.java
@@ -0,0 +1,10 @@
+package Calculadora;
+
+public class Soma {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int soma = num1 + num2;
+        System.out.println(soma);
+    }
+}
